eb3d20e19e8b674fff294a195a2b6113317f19d0
Tomoyuki Sahara <sahara@caddr.net>
2015-02-16 21:00:08 +0900

    sync

--- asm/asm.scm
+++ asm/asm.scm
@@ -1,12 +1,23 @@
+(use binary.io)
+(use gauche.uvector)
+
+
+;; emit one-word instruction, and add something to reloc/...
+(define (assemble-an-instruction line)
+  (case (car line)
+    ((ret) #x00000004)
+    (else (errorf "unknown instruction: ~a" (car line)))))
+
 (define (assemble code out)
   (for-each (lambda (line)
-	      (print line))
+	      (write-u32 (assemble-an-instruction line) out))
 	    code))
 
 (define program
   '((ret)))
 
 (define (main args)
+  (default-endian 'big-endian)
   (call-with-output-file "a.obj"
     (lambda (out)
       (assemble program out))))
