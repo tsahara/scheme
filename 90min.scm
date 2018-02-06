005b8876c9cf2b0c40bae78f09b9501cdfd4b182
Tomoyuki Sahara <sahara@caddr.net>
2018-02-06 23:53:00 +0900

    wip

--- 90min.scm
+++ 90min.scm
@@ -4,6 +4,24 @@
   (until (read) eof-object? => exp
 	 (print exp)))
 
-(define (main args)
+(define (main0 args)
   (with-input-from-file (second args)
     read-toplevel-exps))
+
+(if #f
+    (let loop ((v1 (exp1)))
+      (if (exp2 v1)
+	  #f
+	  (begin
+	    body
+	    (loop (exp1))))))
+
+(let ((exp1 read-line)
+      (exp2 eof-object?))
+  ((lambda (loop) (loop loop (exp1)))
+   (lambda (loop v1)
+     (if (exp2 v1)
+	 #f
+	 (begin
+	   (print v1)
+	   (loop loop (exp1)))))))
